3e8ce5e728333024f1c17a3675bbc2181269beaf
v2panda <pdxuzhen@gmail.com>
2016-08-03 17:39:42 +0800

    update

--- SimpleWeather/jscore/HomePage.js
+++ SimpleWeather/jscore/HomePage.js
@@ -6,7 +6,7 @@
 */ 
 'use strict'
 import React, { Component } from 'react'
-import { View, Animated, Image, TouchableHighlight, Text, StyleSheet } from 'react-native'
+import { View, ScrollView,Alert, Animated, Image, TouchableHighlight, Text, StyleSheet } from 'react-native'
 
 class HomePage extends Component {
    constructor (...args) {
@@ -39,7 +39,50 @@ class HomePage extends Component {
   	} else {
   		content = (
   		<View style={styles.content}>
-  			<Text style={styles.toVideo}>--> 去看视频～</Text>
+        <View style={styles.topcontent}>
+          <TouchableHighlight style={styles.buttonStyle}
+            underlayColor={'#333333'}
+            onPress={() => Alert.alert(
+            'Alert Title',
+            'alertMessage',
+          )}>
+            <Text style={styles.toHistory}>查看</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.buttonStyle1}
+            underlayColor={'#333333'}
+            onPress={() => Alert.alert(
+            'Alert Title',
+            'alertMessage',
+          )}>
+            <Text style={styles.toHistory}>查看</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.buttonStyle1}
+            underlayColor={'#333333'}
+            onPress={() => Alert.alert(
+            'Alert Title',
+            'alertMessage',
+          )}>
+            <Text style={styles.toHistory}>查看</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.buttonStyle1}
+            underlayColor={'#333333'}
+            onPress={() => Alert.alert(
+            'Alert Title',
+            'alertMessage',
+          )}>
+            <Text style={styles.toHistory}>查看</Text>
+          </TouchableHighlight>
+        </View>
+  			<ScrollView contentContainerStyle={styles.scrollView}
+          automaticallyAdjustContentInsets={false}>
+          <Text style={styles.toVideo}>--> 去看视频～</Text>
+          <Text style={styles.toVideo1}>--> 去看视频～</Text>
+          <Text style={styles.toVideo2}>--> 去看视频～</Text>
+          <Text style={styles.toVideo3}>--> 去看视频～</Text>
+          <Text style={styles.toVideo3}>--> 去看视频～</Text>
+          <Text style={styles.toVideo3}>--> 去看视频～</Text>
+          <Text style={styles.toVideo3}>--> 去看视频～</Text>
+        </ScrollView>
   		</View>)
   	}
   	return (
@@ -58,15 +101,80 @@ var styles = StyleSheet.create({
 	},
 	content: {
 		backgroundColor: '#434243',
-    flex: 1
+    flex: 1,
 	},
+  topcontent: {
+    backgroundColor: '#F0FFFF',
+    // flex: 1,
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  scrollView: {
+    backgroundColor: '#4682B4',
+    // flex: 1,
+    // justifyContent: 'space-between',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10
+  },
+  buttonStyle: {
+    backgroundColor: '#87CEEB',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // flex: 1,
+    height:30,
+    width:30,
+    // marginTop: 50,
+    // marginLeft: 10,
+    // marginBottom: 17
+  },
+  buttonStyle1: {
+    backgroundColor: '#87CEEB',
+    height:30,
+    width:30,
+    // marginTop: 50,
+    marginLeft: 50,
+    // marginBottom: 17
+  },
+  toHistory: {
+    fontSize: 10,
+    color: 'white'
+  },
 	toVideo: {
-		fontSize: 14,
+		fontSize: 36,
     color: 'white',
-    position: 'absolute',
-    bottom: 8,
-    right: 15
-	}
+    // marginTop: 50
+    // position: 'absolute',
+    // bottom: 80,
+    // right: 150
+	},
+  toVideo1: {
+    fontSize: 24,
+    color: 'white',
+    marginTop: 50
+    // position: 'absolute',
+    // bottom: 80,
+    // right: 105
+  },
+  toVideo2: {
+    fontSize: 14,
+    color: 'white',
+    marginTop: 50
+    // position: 'absolute',
+    // bottom: 8,
+    // right: 15
+  },
+  toVideo3: {
+    fontSize: 20,
+    color: 'white',
+    marginTop: 100
+    // position: 'absolute',
+    // bottom: 100,
+    // right: 135
+  }
 })
 
 module.exports = HomePage
